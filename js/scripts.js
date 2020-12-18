3976393247cf7560f271de57feb19caaad504b7f
Shervin Firouzdehghan <shervinf@outlook.com>
2020-12-18 19:02:13 -0400

    adjusted body margins

--- js/scripts.js
+++ js/scripts.js
@@ -307,7 +307,6 @@ function intializeGenerationChart(){
                 }]
             },
             options: {
-                
                 scales: {
                     yAxes: [{
                         ticks: {
